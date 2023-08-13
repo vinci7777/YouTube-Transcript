8080a5bd81e251d1f02d9c34c3a8bec15d886c52
vinci777 <krzysiek.fitas91@gmail.com>
2023-08-13 02:01:42 +0200

    init commit msg

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+from sqlalchemy import create_engine, Column, String, Float
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import json
 from youtube_transcript_api import YouTubeTranscriptApi
 
 video_id = "tByVn0whyYc"
-print(YouTubeTranscriptApi.get_transcript(video_id))
+sample_video_transcription = YouTubeTranscriptApi.get_transcript(video_id)
+
+data_string = sample_video_transcription.replace("'", "\"")
+data_list = json.loads(data_string)
+
+# Now you can save the data_list into a database, for example, a JSON file
+with open('data.json', 'w') as json_file:
+    json.dump(data_list, json_file, indent=4)
+
+print("Data saved to data.json")
